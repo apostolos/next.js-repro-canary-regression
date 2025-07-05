2a4d0e6f89b528ef2d14d6fdbaabca3d435b88df
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2025-07-05 15:26:30 +0300

    trigger perf regression

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  experimental: {
+    // Any of the following will trigger the regression:
+    viewTransition: true,
+    // ppr: true,
+    // taint: true,
+  },
 };
 
 export default nextConfig;
